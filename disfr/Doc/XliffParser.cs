3d84b6b3e17f3439ba6409537ce451d3d619b1a2
alissa <alissa_sabre@yahoo.co.jp>
2016-05-17 23:21:49 +0900

    Extracts sdl:cxts from SDL xliff.

--- disfr/Doc/XliffParser.cs
+++ disfr/Doc/XliffParser.cs
@@ -493,14 +493,45 @@ namespace disfr.Doc
                 PhTags = tags?.Where(tag => tag.Element(SDL + "ph") != null)?.ToDictionary(tag => (string)tag.Attribute("id"), tag => tag);
                 PtTags = tags?.Where(tag => tag.Element(SDL + "bpt") != null)?.ToDictionary(tag => (string)tag.Attribute("id"), tag => tag);
                 StTags = tags?.Where(tag => tag.Element(SDL + "st") != null)?.ToDictionary(tag => (string)tag.Attribute("id"), tag => tag);
+
+                Cxts = file.Element(X + "header")?.Element(SDL + "cxt-defs")?.Elements(SDL + "cxt-def")?.ToDictionary(d => (string)d.Attribute("id"), d => (string)d.Attribute("type"));
             }
 
             protected readonly Dictionary<string, string[]> SdlComments;
 
             protected readonly Dictionary<string, XElement> PhTags, PtTags, StTags;
 
+            protected readonly Dictionary<string, string> Cxts;
+
+            protected override IEnumerable<XliffTransPair> ExtractPairs(XElement tu)
+            {
+                // Extract pairs and bind the cxt info to the first segment from this tu.
+                var pairs = base.ExtractPairs(tu);
+                if (Cxts != null && tu.Parent.Name == X + "group")
+                {
+                    var cxts = tu.Parent.Element(SDL + "cxts")?.Elements(SDL + "cxt")?.Select(cxt =>
+                    {
+                        string t;
+                        Cxts.TryGetValue((string)cxt.Attribute("id"), out t);
+                        return t;
+                    })?.Where(t => t != null)?.Distinct();
+                    if (cxts != null)
+                    {
+                        var array = (pairs is XliffTransPair[]) ? (pairs as XliffTransPair[]) : pairs.ToArray();
+                        if (array.Length > 0)
+                        {
+                            var pair = array.FirstOrDefault(p => p.Serial > 0) ?? array[0];
+                            pair.AddProp("sdl:cxt", string.Join("\r\n", cxts));
+                        }
+                        pairs = array;
+                    }
+                }
+                return pairs;
+            }
+
             protected override IEnumerable<XliffTransPair> SegmentAndExtractPairs(XElement tu)
             {
+                // extract segmented pairs and bind various meta info stored in sdl:seg-defs/sdl:seg elements to them.
                 var seginfos = tu.Elements(SDL + "seg-defs").Elements(SDL + "seg").ToDictionary(seg => (string)seg.Attribute("id"));
                 var pairs = base.SegmentAndExtractPairs(tu).ToArray();
                 foreach (var pair in pairs)
