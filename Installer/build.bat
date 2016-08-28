3f7acf0a490b958b8521a81adc20f2987291db19
alissa <alissa_sabre@yahoo.co.jp>
2016-08-28 19:47:03 +0900

    Associated known file types to disfr.

--- Installer/build.bat
+++ Installer/build.bat
@@ -1,4 +1,4 @@
 rem
 echo off
 "%WIX%bin"\candle disfr.wxs
-"%WIX%bin"\light -b . -b ..\disfr\bin\Release -sloc disfr.wixobj
+"%WIX%bin"\light -b . -b ..\disfr\bin\Release -b ..\SHChangeNotify\bin\Release -sloc disfr.wixobj
