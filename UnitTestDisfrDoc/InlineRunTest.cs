e6add13eb8fb8207258adb699c42c76d38505048
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2019-05-30 08:22:28 +0900

    Test for InlineTag.LooseEqualityComparer.

---
+++ UnitTestDisfrDoc/InlineRunTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDisfrDoc
+{
+    [TestClass]
+    public class InlineRunTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
