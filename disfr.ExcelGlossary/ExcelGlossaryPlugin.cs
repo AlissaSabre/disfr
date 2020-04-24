7898a1b02bc2a648bdba93c6d81bb97e0393994a
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2020-04-24 23:56:12 +0900

    ExcelGlossaryPlugin Implemented IPluginStatus to check Excel availability.

--- disfr.ExcelGlossary/ExcelGlossaryPlugin.cs
+++ disfr.ExcelGlossary/ExcelGlossaryPlugin.cs
@@ -14,10 +14,16 @@ namespace disfr.ExcelGlossary
     /// <remarks>
     /// This plugin only supports Excel files in the following format:
     /// </remarks>
-    public class ExcelGlossaryPlugin : IReaderPlugin, IWriterPlugin
+    public class ExcelGlossaryPlugin : IReaderPlugin, IWriterPlugin, IPluginStatus
     {
-        public string Name { get { return "ExcelGlossary"; } }
-        public IReader CreateReader() { return new ExcelGlossaryReader(); }
+        public string Name { get { return "ExcelGlossaryReader"; } }
+
+        private static bool HasExcel = Type.GetTypeFromProgID("Excel.Application") != null;
+
+        public IReader CreateReader() { return HasExcel ? new ExcelGlossaryReader() : null; }
+
         public IWriter CreateWriter() { return null; }
+
+        public string Status { get { return HasExcel ? null : "Excel not found on this computer"; } }
     }
 }
