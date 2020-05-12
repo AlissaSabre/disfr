c3ebe99c9bf5a6b254f6dd4fb9096f5c6de742c8
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2020-05-12 20:52:45 +0900

    Revamp of ExcelGlossary plugin.

--- disfr.ExcelGlossary/ExcelGlossaryReader.cs
+++ disfr.ExcelGlossary/ExcelGlossaryReader.cs
@@ -23,14 +23,19 @@ namespace disfr.ExcelGlossary
 
         public int Priority { get { return 7; } }
 
-        public IAssetBundle Read(string filename, int filterindex)
+        public IAssetBundle Read(string filename, int filterindex_NOTUSED)
         {
             return LoaderAssetBundle.Create(
                 ReaderManager.FriendlyFilename(filename),
-                () => ReadAssets(filename, filterindex));
+                () => ReadAssets(filename));
         }
 
-        private IEnumerable<IAsset> ReadAssets(string filename, int fileterindex)
+        /// <summary>
+        /// Reads ExcelGlossary assets from an Excel book.
+        /// </summary>
+        /// <param name="filename">Full path name of Excel book.</param>
+        /// <returns>A series of ExcelGlossary assets.</returns>
+        private IEnumerable<IAsset> ReadAssets(string filename)
         {
             var list = new List<IAsset>();
             var excel = new Excel() { Visible = false, Interactive = false, DisplayAlerts = false };
@@ -48,7 +53,7 @@ namespace disfr.ExcelGlossary
                     }
                     sheet.Dispose();
                 }
-                return list;
+                return list.Count > 0 ? list : null;
             }
             finally
             {
@@ -57,77 +62,194 @@ namespace disfr.ExcelGlossary
             }
         }
 
+        private static readonly string[] EMPTY_PROPERTY_VALUES = new string[0];
+
         private IAsset ReadFromSheet(Worksheet sheet)
         {
-            // Extract the meaningful range of the sheet.
-            var bottom_right = sheet.Range("A1")
-                .SpecialCells(XlCellType.xlCellTypeLastCell)
-                .Address(true, true, XlReferenceStyle.xlA1);
-            var cells = sheet.Range("A1", bottom_right).Value2 as object[,];
-            var rows = cells.GetLength(0);
-            var columns = cells.GetLength(1);
+            // We need this sheet to be active so that we can access to its window via Application.ActiveWindow.
+            sheet.Activate();
+            var cells = new CellAccessor(sheet);
 
-            // Process headers on the first row.
-            var header = new string[columns];
-            int source_column = -1;
-            int target_column = -1;
-            string source_language = null;
-            string target_language = null;
-            for (int i = 0; i < columns; i++)
+            // Find the header and data rows.
+            int header_row, data_row;
             {
-                var content = cells[1, 1 + i]?.ToString();
-                if (string.IsNullOrWhiteSpace(content))
+                AutoFilter auto_filter = null;
+                Window active_window = null;
+                if ((auto_filter = sheet.AutoFilter) != null)
                 {
-                    var address = sheet.Cells[1, i + 1].Address(true, true, XlReferenceStyle.xlA1);
-                    var column_name = address.Substring(0, address.LastIndexOf('$'));
-                    header[i] = column_name;
+                    // This worksheet has an auto filter.
+                    // Use the filter's header row (i.e., those with filter buttons) as the header,
+                    // and the rows below it as the data.
+                    header_row = auto_filter.Range.Row;
+                    data_row = header_row + 1;
+                }
+                else if ((active_window = sheet.Application.ActiveWindow).FreezePanes)
+                {
+                    // This worksheet has freeze panes.
+                    // Use the last non-empty row on the frozen pane as the header,
+                    // and the non-frozen rows as the data.
+                    var r = active_window.ScrollRow;
+                    var panes = active_window.Panes;
+                    for (int i = 1; i <= panes.Count; i++)
+                    {
+                        var pane = panes[i];
+                        r = Math.Min(r, pane.ScrollRow + pane.VisibleRange.Rows.Count);
+                    }
+                    data_row = r;
+                    while (--r > 1 && cells.IsRowEmpty(r)) ;
+                    header_row = r;
                 }
                 else
                 {
-                    header[i] = content;
-                    var lang = FindLanguageCode(content);
-                    if (lang == null) continue;
-                    if (source_column < 0)
+                    // Otherwise, use the first row as the header (even if it is totally empty),
+                    // and the remainig rows as data.
+                    header_row = 1;
+                    data_row = 2;
+                }
+                active_window?.Dispose();
+                auto_filter?.Dispose();
+            }
+
+            // Fetch column headers on the first row.
+            var headers = new string[cells.Columns + 1];
+            for (int column = 1; column <= cells.Columns; column++)
+            {
+                var label = cells[header_row, column];
+                headers[column] = string.IsNullOrWhiteSpace(label) ? null : label;
+            }
+
+            // Detect source and target languages.
+            // We only consider visible cells for source and target language columns.
+            string source_label = null, source_language = null;
+            string target_label = null, target_language = null;
+            for (int column = 1; column <= cells.Columns; column++)
+            {
+                if (cells.IsCellVisible(header_row, column))
+                {
+                    var lang = FindLanguageCode(headers[column]);
+                    if (lang != null)
                     {
-                        source_column = i;
-                        source_language = lang;
-                    }
-                    else if (target_column < 0)
-                    {
-                        target_column = i;
-                        target_language = lang;
-                    }
-                    else
-                    {
-                        // 3rd or further language codes are handled like a comment column.
+                        if (source_language == null)
+                        {
+                            // Assume the first language code represents the source language.
+                            source_label = headers[column];
+                            source_language = lang;
+                        }
+                        else if (target_language == null && lang != source_language)
+                        {
+                            // Assume the second language code represents the target language.
+                            target_label = headers[column];
+                            target_language = lang;
+                            break;
+                        }
                     }
                 }
             }
-            if (target_column < 0)
+
+            // Group columns into source, target, and other properties.
+            var source_columns = new List<int>();
+            var target_columns = new List<int>();
+            var properties_columns = new List<int>();
+            for (int column = 1; column <= cells.Columns; column++)
             {
-                // this sheet doesn't look like a glossary.
-                return null;
+                var label = headers[column];
+                if (source_label.Equals(label, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    source_columns.Add(column);
+                }
+                else if (target_label.Equals(label, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    target_columns.Add(column);
+                }
+                else
+                {
+                    properties_columns.Add(column);
+                }
             }
 
-            var pairs = new List<ITransPair>();
-            for (int i = 2; i <= rows; i++)
+            // Detect duplicate header labels (i.e., property names)
+            var duplicates = new HashSet<string>(headers.Where(h => h != null).GroupBy(h => h).Where(g => g.Count() > 1).Select(g => g.Key));
+
+            // Modify header labels so that they are unique.
+            // Note that the following modification could produce new ducplicates.
+            // We don't go too strict, because this is just a nice-to-have feature.
+            foreach (var column in properties_columns)
             {
-                var source = cells[i, source_column + 1]?.ToString();
-                var target = cells[i, target_column + 1]?.ToString();
-                if (!string.IsNullOrWhiteSpace(source) && !string.IsNullOrWhiteSpace(target))
+                var label = headers[column];
+                if (label == null)
                 {
-                    pairs.Add(new ExcelGlossaryPair()
+                    // No label.  Use Excel column name as a label.
+                    headers[column] = cells.GetColumnName(column);
+                }
+                else if (duplicates.Contains(label))
+                {
+                    // Duplicate label.  Append the column name.
+                    headers[column] = string.Format("{0} ({1})", label, cells.GetColumnName(column));
+                }
+                else
+                {
+                    // Already unique name.  Leave it alone.
+                }
+            }
+
+            // Turn data rows into TransPairs.
+            var pairs = new List<ITransPair>(cells.Rows); // This capacity is just a rough standard.
+            int serial = 0;
+            var sources = new List<InlineString>(source_columns.Count);
+            var targets = new List<InlineString>(target_columns.Count);
+            var properties = new List<string>(properties_columns.Count);
+            for (int row = data_row; row <= cells.Rows; row++)
+            {
+                if (cells.IsRowEmpty(row))
+                {
+                    // Ignore an empty row.
+                }
+                else
+                {
+                    var visible = cells.IsRowVisible(row);
+                    var id = row.ToString();
+
+                    // Collect source texts.
+                    sources.Clear();
+                    sources.AddRange(source_columns.Select(c => cells[row, c]).Where(s => !string.IsNullOrWhiteSpace(s)).Select(s => new InlineString(s)));
+                    if (sources.Count == 0) sources.Add(InlineString.Empty);
+
+                    // Collect target texts.
+                    targets.Clear();
+                    targets.AddRange(target_columns.Select(c => cells[row, c]).Where(s => !string.IsNullOrWhiteSpace(s)).Select(s => new InlineString(s)));
+                    if (targets.Count == 0) targets.Add(InlineString.Empty);
+
+                    // Collect property values.
+                    properties.Clear();
+                    properties.AddRange(properties_columns.Select(c => cells[row, c]));
+                    int last = properties.Count - 1;
+                    while (last > 0 && string.IsNullOrWhiteSpace(properties[last])) --last;
+                    if (last < properties.Count - 1) properties.RemoveRange(last + 1, properties.Count - last - 1);
+                    var properties_array = properties.Count > 0 ? properties.ToArray() : EMPTY_PROPERTY_VALUES;
+
+                    // Create TransPairs.
+                    // We allow multiple source and/or target texts in an Excel row,
+                    // so we generate all possible source-target combinations.
+                    foreach (var source in sources)
                     {
-                        Serial = pairs.Count + 1,
-                        Id = i.ToString(),
-                        Source = new InlineString(source ?? ""),
-                        Target = new InlineString(target ?? ""),
-                        SourceLang = source_language,
-                        TargetLang = target_language,
-                    });
+                        foreach (var target in targets)
+                        {
+                            pairs.Add(new ExcelGlossaryPair()
+                            {
+                                Serial = visible ? ++serial : -1,
+                                Id = id,
+                                Source = source,
+                                Target = target,
+                                SourceLang = source_language,
+                                TargetLang = target_language,
+                                PropertyValues = properties_array,
+                            });
+                        }
+                    }
                 }
             }
             if (pairs.Count == 0) return null;
+            pairs.TrimExcess();
 
             return new ExcelGlossaryAsset()
             {
@@ -136,13 +258,24 @@ namespace disfr.ExcelGlossary
                 SourceLang = source_language,
                 TargetLang = target_language,
                 TransPairs = pairs,
+                Properties = properties_columns.Select(column => new PropInfo(headers[column], false)).ToArray(),
             };
         }
 
         private CultureInfo[] CultureInfos = null;
 
+        /// <summary>
+        /// Finds a BCP 47 language code for a given user-friendly language designation.
+        /// </summary>
+        /// <param name="text">User friendly name of a language.</param>
+        /// <returns>BCP 47 language code, or null if this method can't find one.</returns>
         private string FindLanguageCode(string text)
         {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return null;
+            }
+
             var cultures = CultureInfos;
             if (cultures == null)
             {
@@ -200,7 +333,7 @@ namespace disfr.ExcelGlossary
 
         public IEnumerable<ITransPair> AltPairs { get { return Enumerable.Empty<ITransPair>(); } }
 
-        public IList<PropInfo> Properties { get { return new PropInfo[0]; } }
+        public IList<PropInfo> Properties { get; internal set; }
     }
 
     class ExcelGlossaryPair : ITransPair
@@ -219,6 +352,11 @@ namespace disfr.ExcelGlossary
 
         public IEnumerable<string> Notes { get { return Enumerable.Empty<string>(); } }
 
-        public string this[int key] { get { return null; } }
+        internal string[] PropertyValues; 
+
+        public string this[int key]
+        {
+            get { return key >= 0 && key < PropertyValues.Length ? PropertyValues[key] : null; }
+        }
     }
 }
