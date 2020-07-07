5a5956479c6c490b88c7af0215932b9db3b95095
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2020-07-07 17:45:14 +0900

    A better inline tag handling in Wordfast XLIFF.

--- disfr-core/Doc/WordfastXliffAsset.cs
+++ disfr-core/Doc/WordfastXliffAsset.cs
@@ -16,5 +16,32 @@ namespace disfr.Doc
             // Wordfast XLIFF seems not supporting XLIFF segmentation.
             SegmentationAllowed = false;
         }
+
+        protected override InlineTag BuildNativeCodeTag(Tag type, XElement element, bool has_code)
+        {
+            // It looks like that a Wordfast XLIFF consistently uses ctype attributes on inline tags,
+            // which is suitable for InlineTag.Display, but only on start tags.
+            // If we see an end tag, try to find the paired start tag to know its ctype.
+            // XLIFF spec says 
+            var display = (string)element.Attribute("ctype");
+            if (display == null && type == Tag.E)
+            {
+                var rid = (string)element.Attribute("rid");
+                display = element.Parent.Elements()
+                    .Where(e => (string)e.Attribute("rid") == rid)
+                    .Select(e => (string)e.Attribute("ctype"))
+                    .FirstOrDefault(s => s != null);
+            }
+
+            // It looks like that a Wordfast XLIFF often has equiv-text attributes on inline tags.
+            return new InlineTag(
+                type: type,
+                id: (string)element.Attribute("id"),
+                rid: (string)element.Attribute("rid") ?? (string)element.Attribute("id"),
+                name:  element.Name.LocalName,
+                ctype: (string)element.Attribute("ctype"),
+                display: display,
+                code: has_code ? element.Value : (string)element.Attribute("equiv-text"));
+        }
     }
 }
