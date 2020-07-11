0c5921ec492494c04da52773277c6ad8b02e0c82
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2020-07-11 22:34:17 +0900

    Inter-segment contents from header/skl/internal-file in Wordfast XLIFF.

--- disfr-core/Doc/WordfastXliffAsset.cs
+++ disfr-core/Doc/WordfastXliffAsset.cs
@@ -15,6 +15,119 @@ namespace disfr.Doc
         {
             // Wordfast XLIFF seems not supporting XLIFF segmentation.
             SegmentationAllowed = false;
+
+            ParseSkeleton(file);
+        }
+
+        protected readonly Dictionary<string, string> InterSegmentsFromSkeleton = new Dictionary<string, string>();
+
+        protected string FirstInterSegmentFromSkeleton;
+
+        private const int MARKER_EXTRA = 4;
+
+        protected void ParseSkeleton(XElement file)
+        {
+            // See if this file has a skeleton.
+            var X = file.Name.Namespace;
+            var skeleton = file.Element(X + "header")?.Element(X + "skl")?.Element(X + "internal-file")?.Value;
+            if (skeleton == null) return;
+
+            var intersegments = InterSegmentsFromSkeleton;
+
+            // Break the skeleton into inter-segment contents by markers.
+            int p = 0;
+            string last_tuid = "";
+            foreach (var group in file.Element(X + "body")?.Descendants(X + "group"))
+            {
+                var id = (string)group.Attribute("id");
+                if (id == null) continue;
+                var q = FindMarker(skeleton, id, p);
+                if (q < 0) continue;
+                if (q > p)
+                {
+                    var tuid = (string)group.Descendants(X + "trans-unit").LastOrDefault()?.Attribute("id");
+                    if (tuid == null) continue;
+                    intersegments[last_tuid] = skeleton.Substring(p, q - p);
+                    last_tuid = tuid;
+                }
+                p = q + id.Length + MARKER_EXTRA;
+            }
+
+            // Record the remnant of the skeleton as the last inter-segment content.
+            if (p < skeleton.Length)
+            {
+                intersegments[last_tuid] = skeleton.Substring(p);
+            }
+
+            // Take care of the first inter-segment content in a special way.
+            intersegments.TryGetValue("", out FirstInterSegmentFromSkeleton);
+        }
+
+        private const char ORM = '\uFFFC';
+
+        protected int FindMarker(string skeleton, string id, int start_index)
+        {
+            var m = "{" + id + "}";
+            var x = skeleton.Length - m.Length - 2;
+            for (int p = start_index + 1, q; ; p = q + m.Length)
+            {
+                q = skeleton.IndexOf(m, p);
+                if (q < 0 || q > x) return -1;
+                if (skeleton[q - 1] == ORM && skeleton[q + m.Length] == ORM) return q - 1;
+            }
+        }
+
+        protected override IEnumerable<XliffTransPair> ExtractPairs(XElement tu)
+        {
+            // TransPairs related to the given tu are collected in this list.
+            var pairs = new List<XliffTransPair>();
+
+            // Add the first inter-segment content from the skeleton before the first trans-unit if any.
+            if (FirstInterSegmentFromSkeleton != null)
+            {
+                pairs.Add(InterSegmentPair(new InlineString(FirstInterSegmentFromSkeleton)));
+                FirstInterSegmentFromSkeleton = null;
+            }
+
+            // Add the _white_spaces_ *before* the segment content.
+            foreach (var ws in tu.Elements(GS + "ws").Where(ws => (string)ws.Attribute("pos") == "before"))
+            {
+                pairs.Add(InterSegmentPair(GetInline(ws)));
+            }
+
+            // Add the segment content.
+            // Note that Wordfast XLIFF appears not employing <mrk mtype="seg"/> segmentation,
+            // so a single translation unit always produces a single pair.
+            pairs.Add(ExtractSinglePair(tu));
+
+            // Add the _white_spaces_ *after* the segment content.
+            foreach (var ws in tu.Elements(GS + "ws").Where(ws => (string)ws.Attribute("pos") == "after"))
+            {
+                pairs.Add(InterSegmentPair(GetInline(ws)));
+            }
+
+            // Add the inter-segment content from the skeleton if any.
+            string intersegment;
+            if (InterSegmentsFromSkeleton.TryGetValue((string)tu.Attribute("id"), out intersegment))
+            {
+                pairs.Add(InterSegmentPair(new InlineString(intersegment)));
+            }
+
+            // That's all from this tu.
+            return pairs;
+        }
+
+        protected XliffTransPair InterSegmentPair(InlineString inline)
+        {
+            return new XliffTransPair()
+            {
+                Serial = -1,
+                Id = "*",
+                Source = inline,
+                Target = inline,
+                SourceLang = SourceLang,
+                TargetLang = TargetLang,
+            };
         }
 
         protected override InlineTag BuildNativeCodeTag(Tag type, XElement element, bool has_code)
