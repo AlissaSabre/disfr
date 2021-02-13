0ee76f0375c9b0cc908f96952a995b9e4215d44c
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2021-02-13 22:16:31 +0900

    disfr.po to have an under-development verison number.

--- disfr.po/Properties/AssemblyInfo.cs
+++ disfr.po/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("disfr.po")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("disfr plugin for gettext po files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alissa Sabre")]
 [assembly: AssemblyProduct("disfr.po")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
