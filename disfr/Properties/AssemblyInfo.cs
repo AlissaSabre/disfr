55508d6469b741d3697cb264ee53ee25adc9b2f4
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2021-02-14 13:50:44 +0900

    Bump versions to 0.4.2.

--- disfr/Properties/AssemblyInfo.cs
+++ disfr/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alissa Sabre")]
 [assembly: AssemblyProduct("disfr")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.1.0")]
-[assembly: AssemblyFileVersion("0.4.1.0")]
+[assembly: AssemblyVersion("0.4.2.0")]
+[assembly: AssemblyFileVersion("0.4.2.0")]
 [assembly: Guid("DFC03605-35FB-49CC-9562-8FC90839BAF1")]
 
